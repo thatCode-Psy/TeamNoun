54a0e89ff64c5b7bf144ff5e41847e174822da39
Jared Okun <okunj20@gmail.com>
2019-01-29 16:34:40 -0500

    fixed jumping

--- Duality/Assets/Scripts/PlayerScript.cs
+++ Duality/Assets/Scripts/PlayerScript.cs
@@ -14,39 +14,69 @@ public class PlayerScript : MonoBehaviour {
     public PlayerColor color;
     public float maxVelocity;
     public float jumpForce;
+    public float collisionOffset = 0.05f;
     
     //Components
     Rigidbody2D rbody;
+    Collider2D collider;
 	
-    bool isGrounded;
     
     // Use this for initialization
 	void Start () {
         rbody = GetComponent<Rigidbody2D>();
-        isGrounded = true;
+        collider = GetComponent<Collider2D>(); 
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
        
         Vector2 velocity = rbody.velocity;
-        velocity.x = Input.GetAxis("Horizontal") * maxVelocity;
+        if(!isHittingWallInDirection())
+            velocity.x = Input.GetAxis("Horizontal") * maxVelocity;
         rbody.velocity = velocity;
-        if(Input.GetAxis("Jump") > 0 && isGrounded) {
+        if(Input.GetAxis("Jump") > 0 && isGrounded()) {
             rbody.AddForce(Vector2.up * jumpForce);
         }
       
 	}
 
-    void OnCollisionStay2D(Collision2D other) {
-        if(rbody.velocity.y == 0) {
-            isGrounded = true;
+    bool isHittingWallInDirection() {
+        float axis = Input.GetAxis("Horizontal");
+        Vector3 min = collider.bounds.min;
+        Vector3 max = collider.bounds.max;
+        if(axis > 0) {
+            max.x += 2 * collisionOffset;
+            min.x = max.x - collisionOffset;
+
         }
+        else if(axis < 0) {
+            min.x -= 2 * collisionOffset;
+            max.x = min.x + collisionOffset;
+        } else {
+            return false;
+        }
+        max.y -= collisionOffset;
+        min.y += collisionOffset;
+
+        return Physics2D.OverlapArea(min, max);
+
+
     }
-    void OnCollisionExit2D(Collision2D other) {
-        if(rbody.velocity.y != 0) {
-            isGrounded = false;
-        }
+
+    bool isGrounded() {
+        Vector3 min = collider.bounds.min;
+        
+        Vector3 max = collider.bounds.max;
+        min.y -= 2 * collisionOffset;
+        max.y = min.y + collisionOffset;
+        max.x -= collisionOffset;
+        min.x += collisionOffset;
+        /*foreach(Collider2D col in Physics2D.OverlapAreaAll(min, max)) {
+            print(col);
+        }*/
+       
+        return Physics2D.OverlapArea(min, max);
     }
 
 }
