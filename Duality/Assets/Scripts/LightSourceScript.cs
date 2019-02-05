c88a233552433372cef9faf9e585776e80cac945
Jared Okun <okunj20@gmail.com>
2019-02-05 03:07:24 -0500

    Light source produces mesh

--- Duality/Assets/Scripts/LightSourceScript.cs
+++ Duality/Assets/Scripts/LightSourceScript.cs
@@ -7,11 +7,17 @@ public class LightSourceScript : MonoBehaviour {
     public float endAngle;
     public int amountOfRayCasts;
 
+    public float raycastRange;
+
     public bool isOn;
     GameObject litArea;
+    public Material litMaterial;
 	// Use this for initialization
 	void Start () {
-		
+		litArea = new GameObject("Light", typeof(MeshFilter), typeof(MeshRenderer));
+        //litArea.transform.parent = transform;
+        litArea.transform.position = Vector2.zero;
+        litArea.GetComponent<MeshRenderer>().material = litMaterial;
 	}
 	
 	// Update is called once per frame
@@ -26,6 +32,39 @@ public class LightSourceScript : MonoBehaviour {
         Vector3[] vertices = new Vector3[amountOfRayCasts + 2];
         vertices[0] = raycastStart;
         Vector2[] uvs = new Vector2[amountOfRayCasts + 2];
+        uvs[0] = Vector2.zero;
+        int[] triangles = new int[amountOfRayCasts * 3];
+
+        float angleDifference = endAngle - startAngle;
         
+        for (int i = 0; i <= amountOfRayCasts; ++i) {
+            float angle = Mathf.Deg2Rad * (startAngle + i * angleDifference / (float)amountOfRayCasts);
+            Vector2 dir = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle));
+            
+            RaycastHit2D raycastHitData2D = Physics2D.Raycast(raycastStart, dir, raycastRange);
+            Debug.DrawRay(raycastStart, dir * raycastHitData2D.distance, Color.white, 0.0f, true);
+
+            if(raycastHitData2D.collider != null){
+                if(raycastHitData2D.collider.tag == "PlayerBlack"){
+                    print("player in area");
+                }
+            }
+          
+            vertices[i + 1] = new Vector2(raycastStart.x, raycastStart.y) + raycastHitData2D.distance * dir;
+            uvs[i + 1] = Vector2.zero;
+
+            if(i != amountOfRayCasts){
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+
+        }
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.uv = uvs;
+        mesh.triangles = triangles;
+        litArea.GetComponent<MeshFilter>().mesh = mesh;
+
     }
 }
