69403ede3a4519bd3abf37edff9edb9629ecf93b
Michael <kellyme213@gmail.com>
2019-02-06 15:45:21 -0500

    add lightswitch model and switch script

---
+++ Duality/Assets/Scripts/lightswitch_script.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightswitch_script : MonoBehaviour
+{
+    public bool light_on = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (light_on)
+        {
+            transform.RotateAround(transform.position, new Vector3(1.0f, 0.0f, 0.0f), 180);
+            //transform.up *= -1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switchLight();
+    }
+
+    void switchLight()
+    {
+        transform.RotateAround(transform.position, new Vector3(1.0f, 0.0f, 0.0f), 180);
+        //transform.up *= -1;
+        light_on = !light_on;
+    }
+}
