96c940c33022bd977dfd31310b603948be501fcc
Michael <kellyme213@gmail.com>
2019-02-07 15:26:17 -0500

    add initial lightswitch script

--- Duality/Assets/Scripts/lightswitch_script.cs
+++ Duality/Assets/Scripts/lightswitch_script.cs
@@ -5,26 +5,40 @@ using UnityEngine;
 public class lightswitch_script : MonoBehaviour
 {
     public bool light_on = false;
+    public List<GameObject> lights = new List<GameObject>();
+
+    //contains the components of all the non-null lights attached to the lightswitch.
+    private List<LightSourceScript> lightComponents = new List<LightSourceScript>();
+
     // Start is called before the first frame update
     void Start()
     {
-        if (light_on)
+        for (int x = 0; x < lights.Count; x++)
         {
-            transform.RotateAround(transform.position, new Vector3(1.0f, 0.0f, 0.0f), 180);
-            //transform.up *= -1;
+            if (lights[x] != null)
+            {
+                LightSourceScript lsc = lights[x].GetComponent<LightSourceScript>();
+                lightComponents.Add(lsc);
+            }
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        switchLight();
+        //switchLight();
     }
 
     void switchLight()
     {
+        //flips the light model upside down so it looks like it gets switched on/off.
         transform.RotateAround(transform.position, new Vector3(1.0f, 0.0f, 0.0f), 180);
-        //transform.up *= -1;
         light_on = !light_on;
+
+        //toggle lights
+        for (int x = 0; x < lightComponents.Count; x++)
+        {
+            lightComponents[x].isOn = !lightComponents[x].isOn;
+        }
     }
 }
