3cf5807c832758687a563cb42ef1e58820e1b6d3
Jared Okun <okunj20@gmail.com>
2019-02-12 20:38:13 -0500

    victory flag works

---
+++ Duality/Assets/Scripts/EndLevelTriggerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevelTriggerScript : MonoBehaviour
+{
+
+    static List<EndLevelTriggerScript> endLevelTriggers = new List<EndLevelTriggerScript>();
+
+    bool triggering;
+    // Start is called before the first frame update
+    void Start()
+    {
+        endLevelTriggers.Add(this);
+        triggering = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider){
+        if(collider.tag == "Player"){
+            triggering = true;
+            CheckWin();
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D collider){
+        if(collider.tag == "Player")
+            triggering = false;
+    }
+
+    void CheckWin(){
+        foreach(EndLevelTriggerScript script in endLevelTriggers){
+            if(!script.triggering){
+                return;
+            }
+        }
+        print("Victory");
+    }
+
+}
