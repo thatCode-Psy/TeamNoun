700e7135b6e6107e5a85e89f96241b3255729fac
Nico Cobb <cobbn@rpi.edu>
2019-04-03 15:09:22 -0400

    input during build bug fix

--- Duality/Assets/Scripts/GameSettings.cs
+++ Duality/Assets/Scripts/GameSettings.cs
@@ -8,7 +8,7 @@ public class GameSettings : MonoBehaviour {
     public bool playMusic = true;
     // so sorry I'm not making this nice and using private variables but who's got the time?
     public InputManager p1InputManager = new InputManager(1, InputManager.ControllerType.Keyboard);
-    public InputManager p2InputManager = new InputManager(2, InputManager.ControllerType.KeyboardTwo);
+    public InputManager p2InputManager = new InputManager(1, InputManager.ControllerType.KeyboardTwo);
 
 	// Use this for initialization
 	void Awake () {
