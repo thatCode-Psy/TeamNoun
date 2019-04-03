1b91c5190d5641f2cc2f2b69bc3d0bd75d7814b7
Unknown <weisea3@rpi.edu>
2019-04-03 00:25:05 -0400

    epilogue done

---
+++ Duality/Assets/Scripts/EpilogueBehavior.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EpilogueBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject camera;
+    public GameObject button;
+    public GameObject lampShade;
+    public float endCamPos;
+    public float lerpThresh;
+    //float t = 0;
+    bool done = false;
+    void Start()
+    {
+        camera = Camera.main.gameObject;
+        button.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+        button.SetActive(false);
+        lampShade.GetComponent<Image>().color = new Color(0, 0, 0, 0);
+        lampShade.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //printf()
+        if(Mathf.Abs(endCamPos - camera.transform.position.y) > lerpThresh)
+            camera.transform.position = new Vector3(camera.transform.position.x, Mathf.Lerp(camera.transform.position.y, endCamPos, Time.deltaTime), camera.transform.position.z);
+        else if(!done)
+        {
+            lampShade.SetActive(true);
+            done = true;
+            StartCoroutine(fadeIn());
+        }
+    }
+
+    IEnumerator fadeIn()
+    {
+        for (float i = 0; i <= 1; i += Time.deltaTime)
+        {
+            lampShade.GetComponent<Image>().color = new Color(0, 0, 0, i);
+            yield return null;
+        }
+        button.SetActive(true);
+        for (float i = 0; i <= 1; i += Time.deltaTime)
+        {
+            button.GetComponent<Image>().color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+    }
+
+    public void Pressed()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
